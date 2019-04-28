e701fba23ff96498c2856f14a08eaae1cf387f65
mcberko <mcberko@hotmail.com>
2019-04-27 17:21:35 -0700

    Update InitialWork.R

--- InitialWork.R
+++ InitialWork.R
@@ -1,1 +1,5 @@
 # Initial Work
+cchs11.new <- cchs11[, c("ADMA_RNO", "CCCA_121", "CCCA_051", "CCCA_05A", "DHHAGAGE", "DHHA_SEX", 
+                      "DHHAGMS", "SDCAGRAC", "SDCAFIMM", "SDCAGRES", "EDUADR04", "INCAGHH", 
+                      "HWTAGBMI", "PACADPAI", "TWDA_5", "SMKADSTY", "ALCADTYP", "CCCA_071", 
+                      "CCCA_101", "CCCA_91B", "FVCADTOT", "GENA_07", "GEOAGPRV", "WTSAM")]
